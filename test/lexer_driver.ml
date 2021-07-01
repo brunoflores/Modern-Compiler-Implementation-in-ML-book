c89a72415d9c5faa7217cb9f6a6a2b042fabfdee
Bruno Flores <bruno@brunoflores.com.br>
2021-07-02 00:39:21 +1000

    Add test driver and a couple of tests.

---
+++ test/lexer_driver.ml
@@ -0,0 +1,28 @@
+open Core
+
+let rec lex_and_print lexbuf =
+  try
+    let token = Lexer.read lexbuf in
+    let out = Token.string_of_token token in
+    Printf.fprintf stdout "%s\n" out;
+    match token with EOF -> () | _ -> lex_and_print lexbuf
+  with Lexer.SyntaxError s ->
+    Printf.fprintf stderr "%s\n" s;
+    exit 1
+
+let get_contents = function
+  | None | Some "-" ->
+      In_channel.input_all In_channel.stdin |> Lexing.from_string
+  | Some filename -> In_channel.create filename |> Lexing.from_channel
+
+let loop filename =
+  let lexbuf = get_contents filename in
+  lex_and_print lexbuf
+
+let command =
+  Command.basic ~summary:"Lex and display the tokens"
+    Command.Let_syntax.(
+      let%map_open filename = anon (maybe ("filename" %: Filename.arg_type)) in
+      fun () -> loop filename)
+
+let () = Command.run command
