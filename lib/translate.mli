8a1161c1482d1a7090e75d19d72366fd543e228b
Bruno Flores <bruno@brunoflores.com.br>
2022-08-06 10:40:47 +1000

    Comment.

--- lib/translate.mli
+++ lib/translate.mli
@@ -1,12 +1,13 @@
-module type S = sig
-  (** Semantic translation. *)
+(** Semantic translation. *)
 
-  (** This module provides functions that are useful in producing intermediate
+(** This module provides functions that are useful in producing intermediate
       representation from abstract syntax. *)
 
-  (** Manages local variables and static function nesting for {!Semant}. *)
+(** Manages local variables and static function nesting for {!Semant}. *)
 
+module type S = sig
   type exp = unit * Tiger.pos option
+  (** The translation of an expression into intermediate code. *)
 
   type level
   (** For function static links. *)
@@ -22,9 +23,11 @@ module type S = sig
   (** Create a new level enclosing the given level. *)
 
   val formals : level -> access list
+  (** TODO document*)
 
   (* val alloc_local : level -> bool -> access *)
   val alloc_local : unit -> access
 end
 
+(** Functor interface. *)
 module Make (Frame : Frame.I) : S
