bf5da143dfc270981ddc4797bb5bf2c069c228aa
Bruno Flores <bruno@brunoflores.com.br>
2022-08-06 14:33:40 +1000

    Compile semant.

--- lib/semant.ml
+++ lib/semant.ml
@@ -13,48 +13,40 @@ module Make
   (* The result is [expty], containing a translated expression and its
      Tiger-language type. *)
   type expty = { exp : Translate.exp; ty : Types.ty }
+  type error = Tiger.pos option * string
+  type errors = error list
+  type ty_res = (expty, errors) result
+  type binop_ty = String | Int
 
-  let rec trans_exp (venv : venv) (tenv : tenv) (exp : Tiger.exp) :
-      (expty, (Tiger.pos option * string) list) result =
-    let rec check_int (e : (expty, (Tiger.pos option * string) list) result) =
-      let check e =
-        let _, pos = e.exp in
-        match e.ty with
-        | Types.Int -> Ok e
-        | _ -> Error (pos, "integer expected")
-      in
+  let rec trans_exp (venv : venv) (tenv : tenv) (exp : Tiger.exp) : ty_res =
+    let rec check_int (e : ty_res) =
       match e with
-      | Ok e -> (
-          match check e with Ok _ as e -> e | Error err -> Error [ err ])
-      | Error _ as errs -> errs
-    (* let rec checkIntBinOp errs left right =
-         let leftty, lefterrs = trexp errs left in
-         let rightty, righterrs = trexp errs right in
-         match (checkInt leftty, checkInt rightty) with
-         | Ok _, Ok _ -> lefterrs @ righterrs
-         | Error err', Error err'' -> err' :: err'' :: lefterrs @ righterrs
-         | _, Error err' | Error err', _ -> err' :: lefterrs @ righterrs
-       and checkBinOp errs left right pos =
-         let leftty, lefterrs = trexp errs left in
-         let rightty, righterrs = trexp errs right in
-         match (leftty, rightty) with
-         | { ty = Types.STRING }, { ty = Types.STRING } -> Ok Types.STRING
-         | { ty = Types.INT }, { ty = Types.INT } -> Ok Types.INT
-         | { ty = Types.INT }, _
-         | { ty = Types.STRING }, _
-         | { ty = Types.NIL }, _
-         | { ty = Types.UNIT }, _
-         | { ty = Types.RECORD (_, _) }, _
-         | { ty = Types.ARRAY (_, _) }, _
-         | { ty = Types.NAME (_, _) }, _ ->
-             Error ((Some pos, "both sides must be string or int") :: errs) *)
-    and trexp (errs : (Tiger.pos option * string) list) (e : Tiger.exp) :
-        (expty, (Tiger.pos option * string) list) result =
+      | Ok expty -> (
+          match expty.ty with
+          | Types.Int -> e
+          | _ -> Error [ (None, "integer required") ])
+      | Error _ -> e
+    and check_int_binop (errs : errors) left right : (unit, errors) result =
+      let lefty = check_int (trexp errs left) in
+      let righty = check_int (trexp errs right) in
+      match (lefty, righty) with
+      | Ok _, Ok _ -> Ok ()
+      | Error err', Error err'' -> Error (err' @ err'' @ errs)
+      | _, Error err' | Error err', _ -> Error (err' @ errs)
+    and check_binop errs left right pos : (binop_ty, errors) result =
+      let lefty = trexp errs left in
+      let righty = trexp errs right in
+      match (lefty, righty) with
+      | Ok { ty = Types.String; _ }, Ok { ty = Types.String; _ } -> Ok String
+      | Ok { ty = Types.Int; _ }, Ok { ty = Types.Int; _ } -> Ok Int
+      | _ -> Error ((Some pos, "both sides must be string or int") :: errs)
+    and trexp (errs : errors) (e : Tiger.exp) : ty_res =
       match e with
       | Tiger.NilExp -> Ok { exp = ((), None); ty = Types.Nil }
       | Tiger.StringExp (_, pos) ->
           Ok { exp = ((), Some pos); ty = Types.String }
       | Tiger.IntExp _ -> Ok { exp = ((), None); ty = Types.Int }
+      | Tiger.SeqExp [] -> failwith ""
       | Tiger.SeqExp ((e, _) :: _) -> trexp errs e
       | Tiger.VarExp (Tiger.SimpleVar (x, pos)) -> (
           match Symbol.look (venv, x) with
@@ -68,17 +60,29 @@ module Make
       | Tiger.LetExp { decs; body; _ } ->
           let venv, tenv = trdec errs decs in
           trans_exp venv tenv body
-      | Tiger.OpExp { left; oper = Tiger.PlusOp; right; pos } -> (
-          let lefty = check_int (trexp errs left) in
-          let righty = check_int (trexp errs right) in
-          match (lefty, righty) with
-          | Ok _, Ok _ -> Ok { exp = ((), Some pos); ty = Types.Int }
-          | (Error _ as l), Ok _ -> l
-          | Ok _, (Error _ as r) -> r
-          | Error l, Error r -> Error (l @ r))
-      | _ -> Error errs
-    and trdec (errs : (Tiger.pos option * string) list) (decs : Tiger.dec list)
-        =
+      | Tiger.OpExp { left; oper = Tiger.PlusOp; right; _ }
+      | Tiger.OpExp { left; oper = Tiger.DivideOp; right; _ }
+      | Tiger.OpExp { left; oper = Tiger.TimesOp; right; _ }
+      | Tiger.OpExp { left; oper = Tiger.MinusOp; right; _ } -> (
+          match check_int_binop errs left right with
+          | Ok _ -> Ok { exp = ((), None); ty = Types.Int }
+          | Error _ as err -> err)
+      | Tiger.OpExp { left; oper = Tiger.GeOp; right; pos }
+      | Tiger.OpExp { left; oper = Tiger.GtOp; right; pos }
+      | Tiger.OpExp { left; oper = Tiger.LeOp; right; pos }
+      | Tiger.OpExp { left; oper = Tiger.LtOp; right; pos }
+      | Tiger.OpExp { left; oper = Tiger.NeqOp; right; pos }
+      | Tiger.OpExp { left; oper = Tiger.EqOp; right; pos } -> (
+          match check_binop errs left right pos with
+          | Ok String -> Ok { exp = ((), Some pos); ty = Types.String }
+          | Ok Int -> Ok { exp = ((), Some pos); ty = Types.Int }
+          | Error _ as err -> err)
+      | Tiger.VarExp var -> trvar errs var
+      | Tiger.CallExp _ | Tiger.RecordExp _ | Tiger.AssignExp _ | Tiger.IfExp _
+      | Tiger.WhileExp _ | Tiger.ForExp _ | Tiger.BreakExp _ | Tiger.ArrayExp _
+        ->
+          failwith ""
+    and trdec (errs : errors) (decs : Tiger.dec list) =
       let venv =
         List.map
           (function
@@ -96,84 +100,36 @@ module Make
              venv
       in
       (venv, tenv)
-      (*
-     | Tiger.OpExp { left; oper = Tiger.DivideOp; right; pos } ->
-         ({ exp = ((), None); ty = Types.INT }, checkIntBinOp errs left right)
-     | Tiger.OpExp { left; oper = Tiger.TimesOp; right; pos } ->
-         ({ exp = ((), None); ty = Types.INT }, checkIntBinOp errs left right)
-     | Tiger.OpExp { left; oper = Tiger.MinusOp; right; pos } ->
-         ({ exp = ((), None); ty = Types.INT }, checkIntBinOp errs left right)
-     | Tiger.OpExp { left; oper = Tiger.GeOp; right; pos } -> (
-         match checkBinOp errs left right pos with
-         | Ok Types.STRING -> ({ exp = ((), Some pos); ty = Types.STRING }, errs)
-         | Ok Types.INT -> ({ exp = ((), Some pos); ty = Types.INT }, errs)
-         | Ok _ -> failwith "cannot happen"
-         | Error errs -> ({ exp = ((), Some pos); ty = Types.INT }, errs))
-     | Tiger.OpExp { left; oper = Tiger.GtOp; right; pos } -> (
-         match checkBinOp errs left right pos with
-         | Ok Types.STRING -> ({ exp = ((), Some pos); ty = Types.STRING }, errs)
-         | Ok Types.INT -> ({ exp = ((), Some pos); ty = Types.INT }, errs)
-         | Ok _ -> failwith "cannot happen"
-         | Error errs -> ({ exp = ((), Some pos); ty = Types.INT }, errs))
-     | Tiger.OpExp { left; oper = Tiger.LeOp; right; pos } -> (
-         match checkBinOp errs left right pos with
-         | Ok Types.STRING -> ({ exp = ((), Some pos); ty = Types.STRING }, errs)
-         | Ok Types.INT -> ({ exp = ((), Some pos); ty = Types.INT }, errs)
-         | Ok _ -> failwith "cannot happen"
-         | Error errs -> ({ exp = ((), Some pos); ty = Types.INT }, errs))
-     | Tiger.OpExp { left; oper = Tiger.LtOp; right; pos } -> (
-         match checkBinOp errs left right pos with
-         | Ok Types.STRING -> ({ exp = ((), Some pos); ty = Types.STRING }, errs)
-         | Ok Types.INT -> ({ exp = ((), Some pos); ty = Types.INT }, errs)
-         | Ok _ -> failwith "cannot happen"
-         | Error errs -> ({ exp = ((), Some pos); ty = Types.INT }, errs))
-     | Tiger.OpExp { left; oper = Tiger.NeqOp; right; pos } -> (
-         match checkBinOp errs left right pos with
-         | Ok Types.STRING -> ({ exp = ((), Some pos); ty = Types.STRING }, errs)
-         | Ok Types.INT -> ({ exp = ((), Some pos); ty = Types.INT }, errs)
-         | Ok _ -> failwith "cannot happen"
-         | Error errs -> ({ exp = ((), Some pos); ty = Types.INT }, errs))
-     | Tiger.OpExp { left; oper = Tiger.EqOp; right; pos } -> (
-         match checkBinOp errs left right pos with
-         | Ok Types.STRING -> ({ exp = ((), Some pos); ty = Types.STRING }, errs)
-         | Ok Types.INT -> ({ exp = ((), Some pos); ty = Types.INT }, errs)
-         | Ok _ -> failwith "cannot happen"
-         | Error errs -> ({ exp = ((), Some pos); ty = Types.INT }, errs))
-     | Tiger.VarExp var ->
-         let expty, varerrs = trvar errs var in
-         (expty, varerrs @ errs) *)
-
-      (* and trvar errs = function
-         | Tiger.SimpleVar (id, pos) -> (
-             match Symbol.look id venv with
-             | Some (Env.VarEntry _) ->
-                 ({ exp = ((), Some pos); ty = Types.NAME (id, ref None) }, errs)
-             | Some (FunEntry _) | None ->
-                 ( { exp = ((), Some pos); ty = Types.INT },
-                   (Some pos, "undefined variable") :: errs ))
-         | Tiger.FieldVar (obj, field, _) -> (
-             match obj with
-             | Tiger.SimpleVar (id, pos) -> (
-                 match Symbol.look id venv with
-                 | Some (Env.VarEntry { ty = Types.RECORD (fields, _) }) -> (
-                     match List.find fields (fun (x, _) -> phys_equal x field) with
-                     | Some _ -> ({ exp = ((), Some pos); ty = Types.INT }, errs)
-                     | None ->
-                         ( { exp = ((), Some pos); ty = Types.INT },
-                           (Some pos, "field is not a member of the record") :: errs
-                         ))
-                 | None ->
-                     ( { exp = ((), Some pos); ty = Types.INT },
-                       (Some pos, "undefined record") :: errs )
-                 | _ -> ({ exp = ((), None); ty = Types.INT }, errs))
-             | _ -> ({ exp = ((), None); ty = Types.INT }, errs))
-         | Tiger.SubscriptVar (_, _, _) ->
-             ({ exp = ((), None); ty = Types.INT }, errs) *)
+    and trvar (errs : errors) (var : Tiger.var) : ty_res =
+      match var with
+      | Tiger.SimpleVar (id, pos) -> (
+          match Symbol.look (venv, id) with
+          | Some (Env.VarEntry _) ->
+              Ok { exp = ((), Some pos); ty = Types.Name (id, ref None) }
+          | Some (FunEntry _) | None ->
+              Error ((Some pos, "undefined variable") :: errs))
+      | Tiger.FieldVar (obj, field, _) -> (
+          match obj with
+          | Tiger.SimpleVar (id, pos) -> (
+              match Symbol.look (venv, id) with
+              | Some (Env.VarEntry { ty = Types.Record (fields, _); _ }) -> (
+                  match List.find_opt (fun (x, _) -> x = field) fields with
+                  | Some _ -> Ok { exp = ((), Some pos); ty = Types.Int }
+                  | None ->
+                      Error
+                        ((Some pos, "field is not a member of the record")
+                        :: errs))
+              | Some _ -> failwith ""
+              | None -> Error ((Some pos, "undefined record") :: errs))
+          | Tiger.FieldVar _ -> failwith ""
+          | Tiger.SubscriptVar _ -> failwith "")
+      | Tiger.SubscriptVar _ -> failwith "not implemented"
     in
 
     trexp [] exp
 
   let trans_prog (exp : Tiger.exp) =
-    let trans_with_base exp = trans_exp Env.base_venv Env.base_tenv exp in
-    match trans_with_base exp with Ok _ -> Ok () | Error _ as err -> err
+    match trans_exp Env.base_venv Env.base_tenv exp with
+    | Ok _ -> Ok ()
+    | Error _ as err -> err
 end
--- lib/tiger.ml
+++ lib/tiger.ml
@@ -1,3 +1,5 @@
+(** The abstract syntax. *)
+
 type pos = { pos_fname : string; pos_lnum : int; pos_bol : int; pos_cnum : int }
 [@@deriving show]
 
