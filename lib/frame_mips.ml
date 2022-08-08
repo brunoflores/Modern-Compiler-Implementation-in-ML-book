88ff9e76e5969281299f428e035d1632e146b117
Bruno Flores <bruno@brunoflores.com.br>
2022-08-09 08:41:13 +1000

    Comment.

--- lib/frame_mips.ml
+++ lib/frame_mips.ml
@@ -4,18 +4,26 @@ module Make : Frame.S = struct
      - Location of all the formals,
      - Instructions required to implement the "view shift",
      - Number of locals allocated so far,
-     - The [label] at which the function's machine code is to begin. *)
+     - The [label] at which the function's machine code is to begin.
+  *)
 
   type access = InFrame of int | InReg of Temp.temp
   (* [InFrame x] indicates a memory location at offset [x] from the
      frame pointer. [InReg r1] indicates that it will be held in
-     "register" [r1]. *)
+     "register" [r1].
+  *)
 
   (* "shift of view", page 136: For each formal parameter, [new_frame]
       must calculate:
       - How the parameter will be seen from inside the function
         (in a register, or in a frame location);
-      - What instructions must be produced to implement the "view shift". *)
+      - What instructions must be produced to implement the "view shift".
+
+      Example: A frame-resident parameter will be seen as "memory at offset
+      X from the frame pointer", and the view shift will be implemented
+      by copying the stack pointer to the frame pointer on entry to the
+      procedure.
+  *)
   let new_frame (_ : Temp.label) (_ : bool list) : frame =
     failwith "not implemented"
 
