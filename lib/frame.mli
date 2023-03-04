8445c969c11b95d904414de1a91597594b8052e2
Bruno Flores <bruno@brunoflores.com.br>
2023-03-04 14:05:29 +1000

    Docs.

--- lib/frame.mli
+++ lib/frame.mli
@@ -1,5 +1,5 @@
 (** An abstract representation of stack frames.
-    Implementation of {!Frame.S} is specific to a target machine. *)
+    Implementations of {!Frame.S} are specific to a target machine. *)
 
 module type S = sig
   type frame
@@ -13,11 +13,12 @@ module type S = sig
 
   val new_frame : Temp.label -> bool list -> frame
   (** To make a new frame for a function [f] with [k] formal parameters,
-      call [new_frame f l], where [l] is a list of [k] booleans: [true] for
-      each parameter that escapes and [false] for each parameter that
-      does not. The result will be a frame object. *)
+      call [new_frame f l], where [l] is a list of formals indicating whether
+      the parameter escapes (needs to be kept in memory) or not.
+      The result is a frame object. *)
 
   val name : frame -> Temp.label
+  (** The frame name. *)
 
   val formals : frame -> access list
   (** The [formals] interface function extracts a list of [k] "accesses"
@@ -25,10 +26,8 @@ module type S = sig
       runtime, as seen from inside the callee. *)
 
   val alloc_local : frame -> bool -> access
-  (** To allocate a new local variable in a frame [f], the semantic analysis
-      phase calls [alloc_local f true].
-
-      The boolean argument to [alloc_local] specifies whether the new variable
-      escapes and needs to go in the frame; if it is [false], then the
-      variable can be allocated in a register. *)
+  (** To allocate a new local variable in a frame. *)
 end
+
+module Make : S
+(** Functor interface. *)
